71d8930438a13208875490e6dedf4e45213d102f
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-02-14 17:15:43 +0900

    ruby_book

---
+++ cherry_book/section/section_4.rb
@@ -0,0 +1,23 @@
+# 配列
+# 基本
+
+# a = [1,2,3]
+# puts a[0]
+
+# 432
+# number = [1,2,3,4]
+# sum = 0
+# number.each do |n|
+#   sum += n
+# end
+
+# puts sum
+
+# 配列の要素を削除する条件を自由に指定する
+# a = [1,2,3,1,2,3]
+# a.delete_if do |n|
+#   n.odd?
+# end
+
+# p a
+
