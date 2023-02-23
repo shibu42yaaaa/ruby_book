849d1b143bf4d9b1d7d633927329279e638f3311
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-02-23 22:23:12 +0900

    mod_section5

--- cherry_book/section/section_5.rb
+++ cherry_book/section/section_5.rb
@@ -1,58 +1,57 @@
 ## ハッシュ　シンボル
 
-# currencies = {'japan'=>'yen', 'us'=>'dollar', 'india'=>'rupee'}
+currencies = {'japan'=>'yen', 'us'=>'dollar', 'india'=>'rupee'}
 
-# currencies['itary'] = 'euro'
+currencies['itary'] = 'euro'
 
-# p currencies
-# # >> {"japan"=>"yen", "us"=>"dollar", "india"=>"rupee", "itary"=>"euro"}
-
-# --
-
-# currencies = {'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
-
-# currencies.each do |key, value|
-#   puts "#{key}:#{value}"
-# end
-## # >> japan:yen
-## >> us:dollar
-## >> india:rupee
+p currencies
+# >> {"japan"=>"yen", "us"=>"dollar", "india"=>"rupee", "itary"=>"euro"}
 
 # --
-## シンボル
 
-# currencies = {:japn => 'yen', :us => 'dollar', :india => 'rupee'}
-# p currencies[:japn]
+currencies = {'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
 
-# currencies = {japan: 'yen', us: 'dollar', india: 'rupee'}
-# p currencies[:us]
+currencies.each do |key, value|
+  puts "#{key}:#{value}"
+end
+# # >> japan:yen
+# >> us:dollar
+# >> india:rupee
 
 # --
-# person = {
-#     name: 'Alice',
-#     age: 24,
-#     friends: ['Bob','Carol'],
-#     phones: {home: '123-4444', mobile: '080-1111-2222'}
-# }
+# シンボル
 
-# p person[:age]
-# p person[:friends]
-# p person[:phones][:mobile]
+currencies = {:japn => 'yen', :us => 'dollar', :india => 'rupee'}
+p currencies[:japn]
+
+currencies = {japan: 'yen', us: 'dollar', india: 'rupee'}
+p currencies[:us]
+
+# --
+person = {
+    name: 'Alice',
+    age: 24,
+    friends: ['Bob','Carol'],
+    phones: {home: '123-4444', mobile: '080-1111-2222'}
+}
+
+p person[:age]
+p person[:friends]
+p person[:phones][:mobile]
 # --
 
 # ハッシュについて応用
 
-# currencies = {japan: 'yen', us: 'dollar', india: 'rupee'}
-# p currencies.has_key?(:japan)
-
+currencies = {japan: 'yen', us: 'dollar', india: 'rupee'}
+p currencies.has_key?(:japan)
 
 # --
 
-# def buy_burger(menu, options = {})
-#     puts options
-# end
+def buy_burger(menu, options = {})
+    puts options
+end
 
-# buy_burger 'fish', {'drink' => true, 'potato' => false}
+buy_burger 'fish', {'drink' => true, 'potato' => false}
 
 # --
 
