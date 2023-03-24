5da8472f3339a0d38d9df1e8eb040ccad6e07859
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-03-24 21:51:19 +0900

    オリジナルコード追加

--- doku_ruby/section8/section8_5.rb
+++ doku_ruby/section8/section8_5.rb
@@ -1,38 +1,38 @@
 # ブロック付きメソッド
 
-# def my_benchmark
-#   start_t = Time.now.to_f
-#   # ブロックの呼び出し
-#   yield
-#   end_t = Time.now.to_f
+def my_benchmark
+  start_t = Time.now.to_f
+  # ブロックの呼び出し
+  yield
+  end_t = Time.now.to_f
 
-#   puts "経過時間:#{end_t - start_t}秒"
-# end
+  puts "経過時間:#{end_t - start_t}秒"
+end
 
-# my_benchmark do
-#   sleep(3)
-# end
+my_benchmark do
+  sleep(3)
+end
 
 #---------------------------------
 
 # 渡されたブロックをeachメソッドで処理する
-# data = ['apple', 'orange', 'remon', 'greap']
+data = ['apple', 'orange', 'remon', 'greap']
 
-# def block_proc(list, &block)
-#   puts 'start....'
-#   list.each &block
-#   puts 'end....'
-# end
+def block_proc(list, &block)
+  puts 'start....'
+  list.each &block
+  puts 'end....'
+end
 
-# block_proc(data) do |item|
-#   puts item
-# end
+block_proc(data) do |item|
+  puts item
+end
 
 #---------------------------------
 
 
-# data = [1.3, 3.3, 1.1, 2.9]
-# p data.map(&:ceil)
+data = [1.3, 3.3, 1.1, 2.9]
+p data.map(&:ceil)
 
 #---------------------------------
 
@@ -57,3 +57,22 @@ end
 
 magic_five_box('HelloWorld', string_proc)
 
+
+# ----------
+#  オリジナル
+# 消費税のメソッド
+def calculate_tax_box(after_input, someProc)
+  someProc.call(1.1, after_input)
+end
+
+
+
+sum_proc = Proc.new do |x,y|
+    # puts "#{@price}"
+    p (x * y).round
+end
+
+puts "値段を入力"
+price = gets.to_i
+
+calculate_tax_box(price, sum_proc)
